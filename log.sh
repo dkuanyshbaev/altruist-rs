a801039c5674fdefb73960c09cdb4bdc18d050d1
Denis K. <konsistentsi@gmail.com>
2025-08-31 22:09:52 +0700

    fix colour

--- log.sh
+++ log.sh
@@ -6,7 +6,7 @@ echo "----------------------------------------"
 
 cat /dev/ttyACM0 | sed -E \
     -e 's/\[SDS011\]/\x1b[34m[SDS011]\x1b[0m/g' \
-    -e 's/\[BME280\]/\x1b[32m[BME280]\x1b[0m/g' \
+    -e 's/\[BME280\]/\x1b[37m[BME280]\x1b[0m/g' \
     -e 's/\[ME2-CO\]/\x1b[36m[ME2-CO]\x1b[0m/g' \
     -e 's/\[AGGREGATOR\]/\x1b[35m[AGGREGATOR]\x1b[0m/g' \
     -e 's/.*(error|timeout|Error|ERROR|backing off).*/\x1b[31m&\x1b[0m/g'
